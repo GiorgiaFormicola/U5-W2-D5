7b4e63659d2a4201a69be4b793d9f484c898c5ac
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-04-17 12:00:38 +0200

    Adds Reservation entity

---
+++ src/main/java/GiorgiaFormicola/U5_W2_D5/entities/Reservation.java
@@ -0,0 +1,44 @@
+package GiorgiaFormicola.U5_W2_D5.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "reservations")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Reservation {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+
+    @Column(name = "request_date", nullable = false)
+    @Setter(AccessLevel.NONE)
+    private LocalDate requestDate;
+
+    @Column(columnDefinition = "text")
+    private String notes;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @OneToOne
+    @JoinColumn(name = "trip_id", nullable = false, unique = true)
+    private Trip trip;
+
+    public Reservation(String notes, Employee employee, Trip trip) {
+        this.requestDate = LocalDate.now();
+        this.notes = notes;
+        this.employee = employee;
+        this.trip = trip;
+    }
+}
