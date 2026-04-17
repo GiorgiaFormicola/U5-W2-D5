32e4bcc8edae26e3f099af299a88546a3f26e4a4
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-04-17 11:46:17 +0200

    Adds Trip entity

---
+++ src/main/java/GiorgiaFormicola/U5_W2_D5/entities/Trip.java
@@ -0,0 +1,39 @@
+package GiorgiaFormicola.U5_W2_D5.entities;
+
+import GiorgiaFormicola.U5_W2_D5.enums.TripStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "trips", uniqueConstraints = {@UniqueConstraint(columnNames = {"destination", "date"})})
+@Getter
+@Setter
+@NoArgsConstructor
+public class Trip {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TripStatus status;
+
+    public Trip(String destination, LocalDate date) {
+        this.destination = destination;
+        this.date = date;
+        this.status = TripStatus.SCHEDULED;
+    }
+}
---
+++ src/main/java/GiorgiaFormicola/U5_W2_D5/enums/TripStatus.java
@@ -0,0 +1,5 @@
+package GiorgiaFormicola.U5_W2_D5.enums;
+
+public enum TripStatus {
+    SCHEDULED, COMPLETED
+}
